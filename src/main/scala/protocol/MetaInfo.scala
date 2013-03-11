59a031bc0947ae4eac6c04eec271490911b0d7f4
Mark Drago <markdrago@gmail.com>
2013-03-11 07:53:35 -0400

    whitespace changes to MetaInfo

--- src/main/scala/protocol/MetaInfo.scala
+++ src/main/scala/protocol/MetaInfo.scala
@@ -2,7 +2,6 @@ package protocol
 
 import akka.util.ByteString
 import java.security.MessageDigest
-import org.apache.commons.codec.net.URLCodec
 import bencoding._
 import scala.Some
 
@@ -12,14 +11,19 @@ class MetaInfo(val dict: BEncodedMap) {
   private def infoMap = dict.get("info").get
 
   def trackerUrl = dict.get("announce").get.toString
+
   def name = infoMap.get("name").get.toString
+
   def pieceLength = infoMap.get("piece length").get.toInt
+
   def isMultifile = infoMap.get("length").isEmpty
+
   def length:Option[Int] = {
     val length = infoMap.get("length")
     if (length.isDefined) { return Some(length.get.toInt)}
     return None
   }
+
   def pieces: List[ByteString] = {
     val bytes = infoMap.get("pieces").get match {
       case s:BEncodedString => s.value
@@ -27,6 +31,7 @@ class MetaInfo(val dict: BEncodedMap) {
     }
     bytes.grouped(20).toList
   }
+
   def files: Option[List[MetaInfoFile]] = {
     val files = infoMap.get("files")
     if (files.isEmpty) { return None }
