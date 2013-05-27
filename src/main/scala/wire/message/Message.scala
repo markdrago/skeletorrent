e9776960a98b15e5c29cfe66c108684e521abc10
Mark Drago <markdrago@gmail.com>
2013-05-27 09:37:44 -0400

    get choke messages parsing & serializing introduce SimpleMessage & SimpleMessageParser

--- src/main/scala/wire/message/Message.scala
+++ src/main/scala/wire/message/Message.scala
@@ -1,12 +1,22 @@
 package wire.message
 
-import akka.util.ByteString
+import akka.util.{ByteStringBuilder, ByteString}
 import java.nio.ByteOrder
 
 trait Message {
   implicit val bo = ByteOrder.BIG_ENDIAN
 
   def serialize: ByteString
+}
+
+//messages of length 1 and no payload
+trait SimpleMessage extends Message {
+  def serializeWithMessageType(messageType: Byte): ByteString = {
+    new ByteStringBuilder()
+      .putInt(1)
+      .putByte(messageType)
+      .result()
+  }
 }
 
 trait MessageParser {
@@ -27,5 +37,17 @@ trait MessageParser {
     //skeletorrent will not support torrents with > 2.1B pieces for example  :-)
     require(result <= Int.MaxValue, "Parsed 4-byte Long claims to be bigger than Int.MaxValue")
     result.toInt
+  }
+}
+
+//message validator for messages with length 1 and no payload
+trait SimpleMessageValidator extends MessageParser {
+  def checkValidForMessageType(str: ByteString, messageType: Byte, typeName: String) {
+    require(str.length >= 5, s"ByteString is too short to be a valid $typeName message")
+
+    val messageLength = fourBytesToInt(str)
+    require(messageLength == 1, s"$typeName messages require claimed size of 1, but $messageLength found")
+
+    require(str(4) == 0, s"$typeName message requires type of $messageType, but ${str(4)} found")
   }
 }
